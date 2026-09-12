f417a4aee24709d9c0f899492bcb26ffd9b68589
Aman <amanxyz.10may@gmail.com>
2026-09-12 15:04:43 +0530

    Iterators

--- vector.cpp
+++ vector.cpp
@@ -5,21 +5,16 @@ using namespace std;
 int main()
 {
 
-    vector<int> vec;
-    vec.push_back(1);
-    vec.push_back(2);
-    vec.push_back(3);
-    vec.push_back(4);
-    vec.push_back(5);
+    vector<int> vec = {1, 2, 3, 4, 5};
 
-    cout << vec.size() << endl;
-    cout << vec.capacity() << endl;
+    vec.clear();
 
     for (int val : vec)
     {
         cout << val << " ";
     }
-    
+    cout << endl;
 
+    cout << vec.size() << " " << vec.capacity();
     return 0;
 }
